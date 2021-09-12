90fad9299a593ac2be9eaee7818012c665503e0d
SimonBurmer <40640328+SimonBurmer@users.noreply.github.com>
2021-09-12 22:47:31 +0200

    Added Flask Server

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("Hallo")
+import unittest
+from app import app
+
+class TestHello(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'<p>Hello, World!</p>')
+
+if __name__ == '__main__':
+    unittest.main()
