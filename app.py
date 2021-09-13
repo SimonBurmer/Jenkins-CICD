9f84dbc417cbf391c852480f158052040d4edb82
SimonBurmer <40640328+SimonBurmer@users.noreply.github.com>
2021-09-13 15:02:01 +0200

    Added routes and tests

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/register")
+def register():
+    return "<p>Klick to Register!</p>"
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 from app import app
 
-class TestHello(unittest.TestCase):
+class TestMainPage(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
@@ -9,7 +9,12 @@ class TestHello(unittest.TestCase):
     def test_hello(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'<p>Hello, World!</p>')
+        self.assertEqual(rv.data, b'<p>Hello, World!</p>')  #b'' converts string to binary 
+
+    def test_register(self):
+        rv = self.app.get('/register')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'<p>Klick to Register!</p>')
 
 if __name__ == '__main__':
     unittest.main()
